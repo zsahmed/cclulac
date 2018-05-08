9c126408d0dfe982241ed88413a64634dba27d92
Zakariya Ahmed <zahmed@credera.com>
2018-05-08 16:18:15 -0700

    adding transition for route changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <app-header></app-header>
-    <router-view></router-view>
+
+    <transition name="fade" mode="out-in" appear>
+      <router-view></router-view>
+    </transition>
+
     <app-footer></app-footer>
   </div>
 </template>
@@ -18,6 +22,21 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.fade-enter {
+  opacity: 0;
+}
 
+.fade-enter-active {
+  transition: opacity 1s;
+}
+
+.fade-leave {
+
+}
+
+.fade-leave-active {
+  transition: opacity .5s;
+  opacity: 0;
+}
 </style>
